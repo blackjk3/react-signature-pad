a703e223fe09e99789ceac3e2bcdf96658bd92c8
Anton Gilgur <agilgur5@gmail.com>
2018-03-31 19:10:36 -0700

    (refactor): Wrap upstream signature_pad
    - Apparently react-signature-pad (what this repo forked from) just
      copy+pasted most of an older version of signature_pad when it was
      first created
    - In order to get all the latest features, fix bugs, and reduce the
      maintenance burden an general surface of this library and its API,
      react-signature-canvas is now a React wrapper around signature_pad
      - It retains the features it added on top of signature_pad, such as
        automatic resizing, a trim feature, and idiomatic React updates
        via props
      - And now has new features like fromDataURL options, toDataURL,
        fromData, and toData! And new props like minDistance and throttle
    
    - Return SignaturePad instance via getSignaturePad
    - Use componentDidMount to trigger prop updates via Object.assign on
      the SignaturePad instance
    
    - Move all wrapper methods to below render for better organization
    
    - Update README with new props and API methods
    - Add links to signature_pad in the props and API sections
    - Change intro to reflect the new changes in this library / repo
    
    - Remove bezier.js and point.js as they are wrapped
    - Remove all mouse / click / touch handling and other core calculations
      that are now wrapped
    - Remove defaultProps that were for the signature_pad implementation
      - This also once again changes up how the dotSize function works
        - It is now back to how it worked pre-v0.2.0
        - Still debatable if this is a bugfix or a breaking change
          (see v0.2.2)
    - Left in propTypes for signature_pad, but maybe those should be
      removed too?

--- README.md
+++ README.md
@@ -11,14 +11,14 @@
 
 [![NPM](https://nodei.co/npm/react-signature-canvas.png?downloads=true&downloadRank=true&stars=true)](https://npmjs.org/package/react-signature-canvas)
 
-A [signature pad](https://github.com/szimek/signature_pad) implementation in
-React.
+A React wrapper component around [signature_pad](https://github.com/szimek/signature_pad).
 
-This is an _unopinionated_ fork of
+Originally, this was just an _unopinionated_ fork of
 [react-signature-pad](https://github.com/blackjk3/react-signature-pad)
-that does not impose any styling or wrap any other unwanted elements around
+that did not impose any styling or wrap any other unwanted elements around
 your canvas -- it's just a wrapper around a single canvas element! Hence the
 naming difference.
+Nowadays, this repo / library has significantly evolved, introducing new features, fixing various bugs, and now wrapping the upstream `signature_pad` to have its updates and bugfixes baked in.
 
 This fork also allows you to directly pass [props](#props) to the underlying
 canvas element, has new, documented [API methods](#api) you can use, and has
@@ -50,9 +50,11 @@ used in drawing. All props are **optional**.
 - `velocityFilterWeight` : `number`, default: `0.7`
 - `minWidth` : `number`, default: `0.5`
 - `maxWidth` : `number`, default: `2.5`
+- `minDistance`: `number`, default: `5`
 - `dotSize` : `number` or `function`,
-  default: `(minWidth, maxWidth) => (minWidth + maxWidth) / 2`
+  default: `() => (this.minWidth + this.maxWidth) / 2`
 - `penColor` : `string`, default: `'black'`
+- `throttle`: `number`, default: `16`
 
 There are also two callbacks that will be called when a stroke ends and one
 begins, respectively.
@@ -70,6 +72,9 @@ Additional props are used to control the canvas element.
 - `clearOnResize`: `bool`, default: `true`
   - whether or not the canvas should be cleared when the window resizes
 
+Of these props, all, except for `canvasProps` and `clearOnResize`, are passed through to `signature_pad` as its [options](https://github.com/szimek/signature_pad#options).
+`signature_pad`'s internal state is automatically kept in sync with prop updates for you (via a `componentDidUpdate` hook).
+
 ### API
 
 All API methods require a ref to the SignatureCanvas in order to use and are
@@ -81,13 +86,21 @@ instance methods of the ref.
 
 - `isEmpty()` : `boolean`, self-explanatory
 - `clear()` : `void`, clears the canvas using the `backgroundColor` prop
-- `fromDataURL(base64String)` : `void`, writes a base64 image to canvas
+- `fromDataURL(base64String, options)` : `void`, writes a base64 image to canvas
+- `toDataURL(mimetype, encoderOptions)`: `base64string`, returns the signature image as a data URL
+- `fromData(pointGroupArray)`: `void`, draws signature image from an array of point groups
+- `toData()`: `pointGroupArray`, returns signature image as an array of point groups
 - `off()`: `void`, unbinds all event handlers
 - `on()`: `void`, rebinds all event handlers
 - `getCanvas()`: `canvas`, returns the underlying canvas ref. Allows you to
   modify the canvas however you want or call methods such as `toDataURL()`
 - `getTrimmedCanvas()`: `canvas`, creates a copy of the canvas and returns a
   trimmed version of it, with all whitespace removed.
+- `getSignaturePad()`: `SignaturePad`, returns the underlying SignaturePad reference.
+
+The API methods are _mostly_ just wrappers around [`signature_pad`'s API](https://github.com/szimek/signature_pad#api).
+`on()` and `off()` will, in addition, bind/unbind the window resize event handler.
+`getCanvas()`, `getTrimmedCanvas()`, and `getSignaturePad()` are new.
 
 ## Example
 
--- src/index.js
+++ src/index.js
@@ -1,70 +1,50 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import SignaturePad from 'signature_pad'
 import trimCanvas from 'trim-canvas'
-
-import Bezier from './bezier.js'
-import Point from './point.js'
 
 export default class SignatureCanvas extends Component {
   static propTypes = {
+    // signature_pad's props
     velocityFilterWeight: PropTypes.number,
     minWidth: PropTypes.number,
     maxWidth: PropTypes.number,
+    minDistance: PropTypes.number,
     dotSize: PropTypes.oneOfType([PropTypes.number, PropTypes.func]),
     penColor: PropTypes.string,
+    throttle: PropTypes.number,
     onEnd: PropTypes.func,
     onBegin: PropTypes.func,
+    // props specific to the React wrapper
     canvasProps: PropTypes.object,
     clearOnResize: PropTypes.bool
   }
 
   static defaultProps = {
-    velocityFilterWeight: 0.7,
-    minWidth: 0.5,
-    maxWidth: 2.5,
-    dotSize: (minWidth, maxWidth) => {
-      return (minWidth + maxWidth) / 2
-    },
-    penColor: 'black',
-    onEnd: () => {},
-    onBegin: () => {},
-    backgroundColor: 'rgba(0,0,0,0)',
     clearOnResize: true
   }
 
-  componentDidMount () {
-    this._ctx = this._canvas.getContext("2d");
+  _sigPad = null
 
-    this.on()
+  _excludeOurProps = () => {
+    let {canvasProps, clearOnResize, ...sigPadProps} = this.props
+    return sigPadProps
+  }
+
+  componentDidMount () {
+    this._sigPad = new SignaturePad(this._canvas, this._excludeOurProps())
     this._resizeCanvas();
     this.clear()
+    this.on()
   }
 
   componentWillUnmount() {
     this.off();
   }
 
-  clear = () => {
-    let ctx = this._ctx
-    let canvas = this._canvas
-
-    ctx.fillStyle = this.props.backgroundColor
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    this._reset()
-    this._isEmpty = true
-  }
-
-  fromDataURL = (dataURL) => {
-    let image = new Image()
-    let ratio = window.devicePixelRatio || 1
-    let width = this._canvas.width / ratio
-    let height = this._canvas.height / ratio
-
-    this._reset()
-    image.onload = () => this._ctx.drawImage(image, 0, 0, width, height)
-    image.src = dataURL
-    this._isEmpty = false
+  // propagate prop updates to SignaturePad
+  componentDidUpdate () {
+    Object.assign(this._sigPad, this._excludeOurProps())
   }
 
   // return the canvas ref for operations like toDataURL
@@ -83,7 +63,10 @@ export default class SignatureCanvas extends Component {
     return trimCanvas(copy)
   }
 
-  isEmpty = () => this._isEmpty
+  // return the internal SignaturePad reference
+  getSignaturePad = () => {
+    return this._sigPad
+  }
 
   _checkClearOnResize = () => {
     if (!this.props.clearOnResize) {
@@ -96,7 +79,6 @@ export default class SignatureCanvas extends Component {
     let canvasProps = this.props.canvasProps || {}
     let {width, height} = canvasProps
 
-    let ctx = this._ctx
     let canvas = this._canvas
     /* When zoomed out to less than 100%, for some very strange reason,
       some browsers report devicePixelRatio as less than 1
@@ -111,245 +93,48 @@ export default class SignatureCanvas extends Component {
       canvas.height = canvas.offsetHeight * ratio
     }
     if(!width || !height) {
-      ctx.scale(ratio, ratio)
+      canvas.getContext('2d').scale(ratio, ratio)
     }
-  }
-
-  _reset = () => {
-    this.points = [];
-    this._lastVelocity = 0;
-    this._lastWidth = (this.props.minWidth + this.props.maxWidth) / 2
-    this._ctx.fillStyle = this.props.penColor
-  }
-
-  _handleMouseEvents = () => {
-    this._mouseButtonDown = false;
-
-    this._canvas.addEventListener('mousedown', this._handleMouseDown)
-    this._canvas.addEventListener('mousemove', this._handleMouseMove)
-    document.addEventListener('mouseup', this._handleMouseUp)
-
-    window.addEventListener('resize', this._checkClearOnResize)
-  }
-
-  _handleTouchEvents = () => {
-    // Pass touch events to canvas element on mobile IE.
-    this._canvas.style.msTouchAction = 'none';
-
-    this._canvas.addEventListener('touchstart', this._handleTouchStart)
-    this._canvas.addEventListener('touchmove', this._handleTouchMove)
-    document.addEventListener('touchend', this._handleTouchEnd)
-  }
-
-  on = () => {
-    this._handleMouseEvents()
-    this._handleTouchEvents()
-  }
-
-  off = () => {
-    this._canvas.removeEventListener('mousedown', this._handleMouseDown)
-    this._canvas.removeEventListener('mousemove', this._handleMouseMove)
-    document.removeEventListener('mouseup', this._handleMouseUp)
-
-    this._canvas.removeEventListener("touchstart", this._handleTouchStart)
-    this._canvas.removeEventListener("touchmove", this._handleTouchMove)
-    document.removeEventListener("touchend", this._handleTouchEnd)
-
-    window.removeEventListener('resize', this._checkClearOnResize)
-  }
-
-  _handleMouseDown = (ev) => {
-    if (ev.which === 1) {
-      this._mouseButtonDown = true
-      this._strokeBegin(ev)
-    }
-  }
-
-  _handleMouseMove = (ev) => {
-    if (this._mouseButtonDown) {
-      this._strokeUpdate(ev)
-    }
-  }
-
-  _handleMouseUp = (ev) => {
-    if (ev.which === 1 && this._mouseButtonDown) {
-      this._mouseButtonDown = false
-      this._strokeEnd(ev)
-    }
-  }
-
-  _handleTouchStart = (ev) => {
-    let touch = ev.changedTouches[0]
-    this._strokeBegin(touch)
-  }
-
-  _handleTouchMove = (ev) => {
-    // prevent scrolling
-    ev.preventDefault()
-
-    let touch = ev.changedTouches[0]
-    this._strokeUpdate(touch)
-  }
-
-  _handleTouchEnd = (ev) => {
-    let wasCanvasTouched = ev.target === this._canvas
-    if (wasCanvasTouched) {
-      this._strokeEnd(ev)
-    }
-  }
-
-  _strokeUpdate = (ev) => {
-    let point = this._createPoint(ev)
-    this._addPoint(point)
-  }
-
-  _strokeBegin = (ev) => {
-    this._reset()
-    this._strokeUpdate(ev)
-    this.props.onBegin(ev)
-  }
-
-  _strokeDraw = (point) => {
-    let ctx = this._ctx
-    let dotSize = typeof(this.props.dotSize) === 'function'
-      ? this.props.dotSize(this.props.minWidth, this.props.maxWidth)
-      : this.props.dotSize
-
-    ctx.beginPath();
-    this._drawPoint(point.x, point.y, dotSize);
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  _strokeEnd = (ev) => {
-    var canDrawCurve = this.points.length > 2,
-        point = this.points[0];
-
-    if (!canDrawCurve && point) {
-      this._strokeDraw(point);
-    }
-
-    this.props.onEnd(ev)
-  }
-
-  _createPoint = (ev) => {
-    let rect = this._canvas.getBoundingClientRect()
-    return new Point(ev.clientX - rect.left, ev.clientY - rect.top)
-  }
-
-  _addPoint = (point) => {
-    var points = this.points,
-        c2, c3,
-        curve, tmp;
-
-    points.push(point);
-
-    if (points.length > 2) {
-      // To reduce the initial lag make it work with 3 points
-      // by copying the first point to the beginning.
-      if (points.length === 3) points.unshift(points[0]);
-
-      tmp = this._calculateCurveControlPoints(points[0], points[1], points[2]);
-      c2 = tmp.c2;
-      tmp = this._calculateCurveControlPoints(points[1], points[2], points[3]);
-      c3 = tmp.c1;
-      curve = new Bezier(points[1], c2, c3, points[2]);
-      this._addCurve(curve);
-
-      // Remove the first element from the list,
-      // so that we always have no more than 4 points in points array.
-      points.shift();
-    }
-  }
-
-  _calculateCurveControlPoints(s1, s2, s3) {
-    var dx1 = s1.x - s2.x, dy1 = s1.y - s2.y,
-        dx2 = s2.x - s3.x, dy2 = s2.y - s3.y,
-
-        m1 = {x: (s1.x + s2.x) / 2.0, y: (s1.y + s2.y) / 2.0},
-        m2 = {x: (s2.x + s3.x) / 2.0, y: (s2.y + s3.y) / 2.0},
-
-        l1 = Math.sqrt(dx1*dx1 + dy1*dy1),
-        l2 = Math.sqrt(dx2*dx2 + dy2*dy2),
-
-        dxm = (m1.x - m2.x),
-        dym = (m1.y - m2.y),
-
-        k = l2 / (l1 + l2),
-        cm = {x: m2.x + dxm*k, y: m2.y + dym*k},
-
-        tx = s2.x - cm.x,
-        ty = s2.y - cm.y;
-
-    return {
-      c1: new Point(m1.x + tx, m1.y + ty),
-      c2: new Point(m2.x + tx, m2.y + ty)
-    };
-  };
-
-  _addCurve = (curve) => {
-    var startPoint = curve.startPoint,
-        endPoint = curve.endPoint,
-        velocity, newWidth;
-
-    velocity = endPoint.velocityFrom(startPoint);
-    velocity = this.props.velocityFilterWeight * velocity
-      + (1 - this.props.velocityFilterWeight) * this._lastVelocity;
-
-    newWidth = this._strokeWidth(velocity);
-    this._drawCurve(curve, this._lastWidth, newWidth);
-
-    this._lastVelocity = velocity;
-    this._lastWidth = newWidth;
-  }
-
-  _drawPoint = (x, y, size) => {
-    var ctx = this._ctx;
-
-    ctx.moveTo(x, y);
-    ctx.arc(x, y, size, 0, 2 * Math.PI, false);
-    this._isEmpty = false;
-  }
-
-  _drawCurve = (curve, startWidth, endWidth) => {
-    var ctx = this._ctx,
-        widthDelta = endWidth - startWidth,
-        drawSteps, width, i, t, tt, ttt, u, uu, uuu, x, y;
-
-    drawSteps = Math.floor(curve.length());
-    ctx.beginPath();
-    for (i = 0; i < drawSteps; i++) {
-        // Calculate the Bezier (x, y) coordinate for this step.
-        t = i / drawSteps;
-        tt = t * t;
-        ttt = tt * t;
-        u = 1 - t;
-        uu = u * u;
-        uuu = uu * u;
-
-        x = uuu * curve.startPoint.x;
-        x += 3 * uu * t * curve.control1.x;
-        x += 3 * u * tt * curve.control2.x;
-        x += ttt * curve.endPoint.x;
-
-        y = uuu * curve.startPoint.y;
-        y += 3 * uu * t * curve.control1.y;
-        y += 3 * u * tt * curve.control2.y;
-        y += ttt * curve.endPoint.y;
-
-        width = startWidth + ttt * widthDelta;
-        this._drawPoint(x, y, width);
-    }
-    ctx.closePath();
-    ctx.fill();
-  }
-
-  _strokeWidth = (velocity) => {
-    return Math.max(this.props.maxWidth / (velocity + 1), this.props.minWidth)
   }
 
   render () {
     let {canvasProps} = this.props
     return <canvas ref={(ref) => { this._canvas = ref }} {...canvasProps} />
+  }
+
+
+  // all wrapper functions below render
+  on = () => {
+    window.addEventListener('resize', this._checkClearOnResize)
+    return this._sigPad.on()
+  }
+
+  off = () => {
+    window.removeEventListener('resize', this._checkClearOnResize)
+    return this._sigPad.off()
+  }
+
+  clear = () => {
+    return this._sigPad.clear()
+  }
+
+  isEmpty = () => {
+    return this._sigPad.isEmpty()
+  }
+
+  fromDataURL = (dataURL, options) => {
+    return this._sigPad.fromDataURL(dataURL, options)
+  }
+
+  toDataURL = (type, encoderOptions) => {
+    return this._sigPad.toDataURL(type, encoderOptions)
+  }
+
+  fromData = (pointGroups) => {
+    return this._sigPad.fromData(pointGroups)
+  }
+
+  toData = () => {
+    return this._sigPad.toData()
   }
 }
