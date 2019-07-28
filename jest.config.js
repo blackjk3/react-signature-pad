94fb45be9f6b3911007f98f24fb7f37175287ce6
Anton Gilgur <agilgur5@gmail.com>
2019-07-28 17:30:03 -0400

    (test): add code coverage configuration
    - ignore test-utils/ directory in coverage
    - update gitignore with coverage/ directory
      - and fix missing newline

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,9 @@ module.exports = {
   transform: {
     // use babel-jest@23 for babel@6 support (https://github.com/facebook/jest/issues/8230#issuecomment-479470547)
     '\\.js$': require.resolve('babel-jest')
-  }
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/', // default
+    '<rootDir>/test-utils/' // ignore test-utils
+  ]
 }
