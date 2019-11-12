e577de3fb3c96d5d7fc055d091e136450b459b25
Khoa Thai <khoa.thai@employmenthero.com>
2019-11-13 10:04:16 +1100

    Map with eh

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import SignaturePad from 'signature_pad'
-import trimCanvas from 'trim-canvas'
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import SignaturePad from "signature_pad";
+import trimCanvas from "trim-canvas";
 
 export default class SignatureCanvas extends Component {
   static propTypes = {
@@ -18,124 +18,131 @@ export default class SignatureCanvas extends Component {
     // props specific to the React wrapper
     canvasProps: PropTypes.object,
     clearOnResize: PropTypes.bool
-  }
+  };
 
   static defaultProps = {
     clearOnResize: true
-  }
+  };
 
-  _sigPad = null
+  _sigPad = null;
 
   _excludeOurProps = () => {
-    const { canvasProps, clearOnResize, ...sigPadProps } = this.props
-    return sigPadProps
+    const { canvasProps, clearOnResize, ...sigPadProps } = this.props;
+    return sigPadProps;
+  };
+
+  componentDidMount() {
+    this._sigPad = new SignaturePad(this._canvas, this._excludeOurProps());
+    this._resizeCanvas();
+    this.on();
   }
 
-  componentDidMount () {
-    this._sigPad = new SignaturePad(this._canvas, this._excludeOurProps())
-    this._resizeCanvas()
-    this.on()
-  }
-
-  componentWillUnmount () {
-    this.off()
+  componentWillUnmount() {
+    this.off();
   }
 
   // propagate prop updates to SignaturePad
-  componentDidUpdate () {
-    Object.assign(this._sigPad, this._excludeOurProps())
+  componentDidUpdate() {
+    Object.assign(this._sigPad, this._excludeOurProps());
   }
 
   // return the canvas ref for operations like toDataURL
   getCanvas = () => {
-    return this._canvas
-  }
+    return this._canvas;
+  };
 
   // return a trimmed copy of the canvas
   getTrimmedCanvas = () => {
     // copy the canvas
-    const copy = document.createElement('canvas')
-    copy.width = this._canvas.width
-    copy.height = this._canvas.height
-    copy.getContext('2d').drawImage(this._canvas, 0, 0)
+    const copy = document.createElement("canvas");
+    copy.width = this._canvas.width;
+    copy.height = this._canvas.height;
+    copy.getContext("2d").drawImage(this._canvas, 0, 0);
     // then trim it
-    return trimCanvas(copy)
-  }
+    return trimCanvas(copy);
+  };
 
   // return the internal SignaturePad reference
   getSignaturePad = () => {
-    return this._sigPad
-  }
+    return this._sigPad;
+  };
 
   _checkClearOnResize = () => {
     if (!this.props.clearOnResize) {
-      return
+      return;
     }
-    this._resizeCanvas()
-  }
+    this._resizeCanvas();
+  };
 
   _resizeCanvas = () => {
-    const canvasProps = this.props.canvasProps || {}
-    const { width, height } = canvasProps
+    const canvasProps = this.props.canvasProps || {};
+    const { width, height } = canvasProps;
     // don't resize if the canvas has fixed width and height
     if (width && height) {
-      return
+      return;
     }
 
-    const canvas = this._canvas
+    const canvas = this._canvas;
     /* When zoomed out to less than 100%, for some very strange reason,
       some browsers report devicePixelRatio as less than 1
       and only part of the canvas is cleared then. */
-    const ratio = Math.max(window.devicePixelRatio || 1, 1)
+    const ratio = Math.max(window.devicePixelRatio || 1, 1);
 
     if (!width) {
-      canvas.width = canvas.offsetWidth * ratio
+      canvas.width = canvas.offsetWidth * ratio;
     }
     if (!height) {
-      canvas.height = canvas.offsetHeight * ratio
+      canvas.height = canvas.offsetHeight * ratio;
     }
-    canvas.getContext('2d').scale(ratio, ratio)
-    this.clear()
-  }
+    canvas.getContext("2d").scale(ratio, ratio);
+    this.clear();
+  };
 
-  render () {
-    const { canvasProps } = this.props
-    return <canvas ref={(ref) => { this._canvas = ref }} {...canvasProps} />
+  render() {
+    const { canvasProps } = this.props;
+    return (
+      <canvas
+        ref={ref => {
+          this._canvas = ref;
+        }}
+        {...canvasProps}
+      />
+    );
   }
 
   // all wrapper functions below render
   //
   on = () => {
-    window.addEventListener('resize', this._checkClearOnResize)
-    return this._sigPad.on()
-  }
+    window.addEventListener("resize", this._checkClearOnResize);
+    return this._sigPad.on();
+  };
 
   off = () => {
-    window.removeEventListener('resize', this._checkClearOnResize)
-    return this._sigPad.off()
-  }
+    window.removeEventListener("resize", this._checkClearOnResize);
+    return this._sigPad.off();
+  };
 
   clear = () => {
-    return this._sigPad.clear()
-  }
+    return this._sigPad.clear();
+  };
 
   isEmpty = () => {
-    return this._sigPad.isEmpty()
-  }
+    return this._sigPad.isEmpty();
+  };
 
   fromDataURL = (dataURL, options) => {
-    return this._sigPad.fromDataURL(dataURL, options)
-  }
+    return this._sigPad.fromDataURL(dataURL, options);
+  };
 
   toDataURL = (type, encoderOptions) => {
-    return this._sigPad.toDataURL(type, encoderOptions)
-  }
+    return this._sigPad.toDataURL(type, encoderOptions);
+  };
 
-  fromData = (pointGroups) => {
-    return this._sigPad.fromData(pointGroups)
-  }
+  fromData = pointGroups => {
+    return this._sigPad.fromData(pointGroups);
+  };
 
   toData = () => {
-    return this._sigPad.toData()
-  }
+    return this._sigPad.toData();
+  };
 }
